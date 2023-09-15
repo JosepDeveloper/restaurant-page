09ec1690e07e08b5a887c7a83929a466d8a2b552
Jose Oviedo <thejm1133@gmail.com>
2023-09-15 17:24:30 -0400

    Adding GitHub image to project

--- src/componenst/footer.js
+++ src/componenst/footer.js
@@ -1,5 +1,17 @@
 export const footer = `
   <div class="footer">
-    JosepDeveloper on <a href="#" target="_blank">GitHub</a>
+    JosepDeveloper on 
+    <a
+      href="https://github.com/josepDeveloper/"
+      target="_blank"
+      class="hvr-grow"
+    >
+      <img
+          src="./public/img/GitHub_Logo_White.png"
+          alt="GitHub Logo"
+          height="auto"
+          width="100"
+      />
+    </a>
   </div>
 `;
